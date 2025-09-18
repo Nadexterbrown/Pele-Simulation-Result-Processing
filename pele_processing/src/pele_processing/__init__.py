3e5dc38fea356774972da70c4b6516971808d22f
Nadexterbrown <nadexterbrown@gmail.com>
2025-09-18 00:48:18 -0400

    updated package import behavior

---
+++ pele_processing/src/pele_processing/__init__.py
@@ -0,0 +1,14 @@
+"""
+Pele Processing Package - Unified interface for base and additional processing.
+
+This package combines core Pele simulation processing with advanced analysis tools.
+"""
+
+# Import everything from base_processing
+from base_processing import *
+
+# Import everything from additional_processing
+from additional_processing import *
+
+# Import version info from base_processing
+from base_processing import __version__, __author__, __email__, __description__, __url__
